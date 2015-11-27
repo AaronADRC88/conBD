92e720496c4c64b16e8db4e1022abfb0c7539fee
aferreiradominguez <aferreiradominguez@atanasoff07.danielcastelao.org>
2015-11-27 10:52:04 +0100

    conecta a bd

---
+++ conn.py
@@ -0,0 +1,28 @@
+__author__ = 'aferreiradominguez'
+import sqlite3 as dbapi
+#version api
+print(dbapi.apilevel)
+#0-3 nivel seguridade do modulo para usar fios
+print(dbapi.threadsafety)
+#Indica sintaxe para a insercion de param
+print(dbapi.paramstyle)
+
+bbdd=dbapi.connect("basedatos.dat")
+
+print(bbdd)
+
+cursor=bbdd.cursor()
+
+#cursor.execute("""create table empleados (dni text primary key, nome text ,departamento text,sueldo integer)""")
+
+#cursor.execute("""insert into empleados values('111111111-Z','paco','informatico',1500)""")
+
+#cursor.execute("""insert into empleados values('111111112-Z','lolololo','celtista',3000)""")
+
+
+#bbdd.commit()
+
+cursor.execute("""select * from empleados""")
+for result in cursor:
+    print("Dni: "+result[0]+"nome: "+result[1]+"profesion: "+result[2]+"sueldo: "+str(result[3]))
+bbdd.close()
